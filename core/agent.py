07211cadc1aa5198a80508fb95d40e1172b656f9
erinaudo-fabric <erinaudo@fabricdata.com>
2025-09-24 16:38:30 -0300

    rename tools.py -> all_tools.py

--- core/agent.py
+++ core/agent.py
@@ -9,7 +9,7 @@ import psycopg
 
 from src.sql.utils.sql_db import SQLConnectionManager  
 from src.prompt_templates.prompt import prompt as SYSTEM_PROMPT
-from src.sql.tools.tools import ALL_SQL_TOOLS
+from sql.tools.all_tools import ALL_SQL_TOOLS
 
 VERBOSE = True
 

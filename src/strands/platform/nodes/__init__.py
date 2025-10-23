e498fbe4f5b0441ef1f844efe25742f3da7ca4af
erinaudo-fabric <erinaudo@fabricdata.com>
2025-10-23 08:47:01 -0300

    del deprec folder and script

--- src/strands/platform/nodes/__init__.py
+++ src/strands/platform/nodes/__init__.py
@@ -1,11 +1,8 @@
 
 from .availability import availability_node
 from .presence import presence_node
-from .routers import route_availability_tool, route_presence_tool
 
 __all__ = [
     'availability_node',
-    'presence_node',
-    'route_availability_tool',
-    'route_presence_tool'
+    'presence_node'
 ]
